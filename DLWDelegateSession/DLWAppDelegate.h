4c9087d718d04dc5ad1d2c5d87434dd5e1524316
Guangming Li <leeful@gmail.com>
2013-10-13 09:34:42 +0800

    Initial Commit

---
+++ DLWDelegateSession/DLWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DLWAppDelegate.h
+//  DLWDelegateSession
+//
+//  Created by dengliwa on 13-10-13.
+//  Copyright (c) 2013年 dengliwa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DLWDelegateSession/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DLWDelegateSession
+//
+//  Created by dengliwa on 13-10-13.
+//  Copyright (c) 2013年 dengliwa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DLWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DLWAppDelegate class]));
+    }
+}
